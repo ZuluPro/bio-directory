65eaa5763101c16b4a2e4c5a114d5086632b7451
ZuluPro <montheanthony@hotmail.com>
2016-07-19 15:16:52 -0400

    Fix migrations

--- bio/items/migrations/0001_initial.py
+++ bio/items/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bio', '0004_auto_20160704_1419'),
+        ('bio', '0001_initial'),
     ]
 
     operations = [
